6170041565e9f4a70809299cc49893276435fb25
GojkoJapundza <131908823+GojkoJapundza@users.noreply.github.com>
2023-05-05 20:15:09 +0200

    Contact JS

---
+++ client/js/contact.js
@@ -0,0 +1,62 @@
+let lang=sessionStorage.getItem("lang");
+
+const getTextEN = async () => {
+    const res = await fetch("http://localhost:8008/contactTextEn");
+    const data = await res.json();
+
+    return data;
+}
+
+const getTextSR = async () => {
+    const res = await fetch("http://localhost:8008/contactTextSR");
+    const data = await res.json();
+
+    return data;
+}
+
+if(lang=="en"){
+    getTextEN().then(texts => {
+        texts.forEach(text => {
+            console.log(text.div_id);
+            console.log(text.content);
+            document.getElementById(text.div_id).innerHTML = text.content;
+            console.log(text);
+        })
+    });
+}
+if(lang=="sr"){
+    getTextSR().then(texts => {
+        texts.forEach(text => {
+            document.getElementById(text.div_id).innerHTML = text.content;
+            console.log(text);
+        })
+    });
+}
+
+document.getElementById('lang_en').addEventListener('click', () => {
+    lang = "en";
+    sessionStorage.setItem("lang", lang);
+    console.log(lang);
+
+    getTextEN().then(texts => {
+        texts.forEach(text => {
+            console.log(text.div_id);
+            console.log(text.content);
+            document.getElementById(text.div_id).innerHTML = text.content;
+            console.log(text);
+        });
+    });
+});
+
+document.getElementById('lang_sr').addEventListener('click', () => {
+    lang = "sr";
+    sessionStorage.setItem("lang", lang);
+    console.log(lang);
+
+    getTextSR().then(texts => {
+        texts.forEach(text => {
+            document.getElementById(text.div_id).innerHTML = text.content;
+            console.log(text);
+        });
+    });
+});
